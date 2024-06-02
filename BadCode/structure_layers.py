ca58e21818bdf6b81040bd0e98d3cec947bbe562
Etern1ty <Sandr1109@yandex.ru>
2024-06-02 17:19:56 +0400

    ldsd

--- BadCode/structure_layers.py
+++ BadCode/structure_layers.py
@@ -3,7 +3,9 @@ from torchsummary import summary
 from ultralytics import YOLO
 
 # Загрузка модели YOLOv8
-model = YOLO('yolov8s.pt')
+model = YOLO("Weights/yolov8s.pt")
 
 # Вывод структуры модели
-summary(model, input_size=(3, 640, 640))
+# summary(model, input_size=(3, 640, 640))
+model.info(True)
+
